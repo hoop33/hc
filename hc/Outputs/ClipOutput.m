c643622218ac10b878542108d301cd6c1feb2bd3
Rob Warner <rwarner@grailbox.com>
2014-12-12 08:55:39 -0500

    Add output to clipboard

---
+++ hc/Outputs/ClipOutput.m
@@ -0,0 +1,32 @@
+//
+// Created by Rob Warner on 12/12/14.
+// Copyright (c) 2014 Rob Warner. All rights reserved.
+//
+
+@import AppKit;
+
+#import "ClipOutput.h"
+#import "Color.h"
+#import "Response.h"
+
+@implementation ClipOutput
+
+- (void)output:(Response *)response {
+  NSMutableString *text = [NSMutableString string];
+  for (Color *color in response.colors) {
+    [text appendFormat:@"%@\n", [color description]];
+  }
+  if (response.message != nil) {
+    [text appendString:response.message];
+  }
+
+  NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
+  [pasteboard clearContents];
+  [pasteboard writeObjects:@[text]];
+}
+
+- (NSString *)summary {
+  return @"Outputs to the clipboard";
+}
+
+@end
